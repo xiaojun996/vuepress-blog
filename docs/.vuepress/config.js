22d0fc922858d096767a1438c0fd8aec1d6fbb8d
xiaojun996 <blueskyzx@icloud.com>
2020-08-04 23:27:02 +0800

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,27 @@ const nav = require('./config/config.nav')
 
 module.exports = {
   title: 'xiaojun996',
-  description: 'Just playing around',
+  description: 'haha~',
+  head: [
+    // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }], // favicons，资源放在public文件夹
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content:
+          '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
+      },
+    ],
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    [
+      'meta',
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no',
+      },
+    ], // 移动端阻止页面缩放
+  ],
   theme: 'vdoing', // 使用依赖包主题
   themeConfig: {
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
@@ -45,6 +65,7 @@ module.exports = {
     footer: {
       // 页脚信息
       createYear: 2019, // 博客创建年份
+      copyrightInfo: 'xiaojun996',
     },
   },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,14 +1,7 @@
 ---
 home: true
-tagline: Hero 我的个人博客
-actionText: 查看个人资料 →
-actionLink: /about/
-features:
-  - title: 简洁至上
-    details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
-  - title: Vue驱动
-    details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
-  - title: 高性能
-    details: VuePress 为每个页面预渲染生成静态的 HTML，同时在页面被加载的时候，将作为 SPA 运行。
-footer: Copyright © 2020-present Xiaojun996
+heroText: xiaojun996 blog
+tagline: hey~ 这里是我个人小天地
+actionText: 查看我的信息 →
+actionLink: /about/about.md
 ---
