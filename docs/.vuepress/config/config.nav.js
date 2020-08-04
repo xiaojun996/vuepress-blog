d3fe3e8226c6ac9c21374a847af5164694872422
xiaojun996 <blueskyzx@icloud.com>
2020-08-04 23:49:16 +0800

    Update

--- docs/.vuepress/config/config.nav.js
+++ docs/.vuepress/config/config.nav.js
@@ -2,12 +2,12 @@ module.exports = [
   {
     text: '学习笔记',
     items: [
-      { text: 'JavaScript', link: '/javascript/' },
-      { text: 'Vue', link: '/vue/' },
-      { text: 'CSS', link: '/css/' },
+      { text: 'JavaScript', link: '/pages/6e2f5c/' },
+      { text: 'Vue', link: '/pages/4331e9/' },
+      { text: 'CSS', link: '/pages/b9190a/' },
     ],
   },
-  { text: '关于', link: '/about/01.about.md' },
+  { text: '关于', link: '/pages/10790c/' },
   { text: 'Twitter', link: 'https://twitter.com/Luka_ai', target: '_blank' },
   { text: 'GitHub', link: 'https://github.com/xiaojun996/', target: '_blank' },
 ]
