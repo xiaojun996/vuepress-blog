628bee641da412891093af65084c279135717561
xiaojun996 <blueskyzx@icloud.com>
2020-09-20 02:19:22 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,27 +25,31 @@ module.exports = {
     ], // 移动端阻止页面缩放
   ],
   plugins: [
-    ['thirdparty-search', {
-      thirdparty: [ // 可选，默认 []
-        {
-          title: '在MDN中搜索', // 在搜索结果显示的文字
-          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-          behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
-        },
-        {
-          title: '在Runoob中搜索',
-          frontUrl: 'https://www.runoob.com/?s=',
-        },
-        {
-          title: '在Vue API中搜索',
-          frontUrl: 'https://cn.vuejs.org/v2/api/#',
-        },
-        {
-          title: '通过百度搜索本站的',
-          frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20'
-        }
-      ]
-    }], 
+    [
+      'thirdparty-search',
+      {
+        // 可选，默认 []
+        thirdparty: [
+          {
+            title: '在MDN中搜索', // 在搜索结果显示的文字
+            frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+            behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
+          },
+          {
+            title: '在Runoob中搜索',
+            frontUrl: 'https://www.runoob.com/?s=',
+          },
+          {
+            title: '在Vue API中搜索',
+            frontUrl: 'https://cn.vuejs.org/v2/api/#',
+          },
+          {
+            title: '通过百度搜索本站的',
+            frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
+          },
+        ],
+      },
+    ],
   ],
   theme: 'vdoing', // 使用依赖包主题
   themeConfig: {
