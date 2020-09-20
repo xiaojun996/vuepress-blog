becd6db6a4b115f509d2e3b96be4538004cb32f5
xiaojun996 <blueskyzx@icloud.com>
2020-09-20 11:55:21 +0800

    feat: add one-click-copy 复制代码插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -51,6 +51,16 @@ module.exports = {
       },
     ],
     [
+      'one-click-copy',
+      {
+        // 代码块复制按钮
+        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+        duration: 1000, // prompt message display time.
+        showInMobile: false, // whether to display on the mobile side, default: false.
+      },
+    ],
+    [
       'vuepress-plugin-comment',
       {
         choosen: 'gitalk',
@@ -62,7 +72,7 @@ module.exports = {
           admin: ['xiaojun996'],
           distractionFreeMode: false,
           pagerDirection: 'last', // 'first'正序 | 'last'倒序
-          labels: ["Gitalk", "Comment"], // GitHub issue 的标签
+          labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
         },
       },
     ],
