a02f742e86c739bb929d45d65416c415594989d6
xiaojun996 <blueskyzx@icloud.com>
2020-09-21 21:38:32 +0800

    fix: 优化首次打开背景图卡顿

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,4 @@
+const head = require('./config/config.head')
 const nav = require('./config/config.nav')
 const plugins = require('./config/config.plugins')
 const theme = require('./config/config.theme')
@@ -6,37 +7,7 @@ module.exports = {
   title: 'xiaojun996',
   description: 'haha~',
   dest: './dist',
-  /**
-   * 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-   */
-  head: [
-    [
-      // favicons，资源放在public文件夹
-      'link',
-      { rel: 'icon', href: '/img/favicon.ico' },
-    ],
-    [
-      'meta',
-      {
-        name: 'keywords',
-        content:
-          '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
-      },
-    ],
-    [
-      // 移动浏览器主题颜色
-      'meta',
-      { name: 'theme-color', content: '#11a8cd' },
-    ],
-    [
-      // 移动端阻止页面缩放
-      'meta',
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no',
-      },
-    ],
-  ],
+  head,
   plugins,
   theme: 'vdoing', // 使用依赖包主题
   themeConfig: {
---
+++ docs/.vuepress/config/config.head.js
@@ -0,0 +1,33 @@
+/**
+ * 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+ */
+module.exports = [
+  // favicons，资源放在public文件夹
+  [ 'link', { rel: 'icon', href: '/img/favicon.ico' } ],
+  ['link', { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/688669.png', as: 'image' }],
+  ['link', { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/751262.png', as: 'image' }],
+  ['link', { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/843699.png', as: 'image' }],
+  ['link', { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/862789.png', as: 'image' }],
+  ['link', { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/917480.png', as: 'image' }],
+  ['link', { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/998208.png', as: 'image' }],
+  ['link', { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/999332.png', as: 'image' }],
+  ['link', { rel: 'preload', href: 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/1049978.png', as: 'image' }],
+  [
+    'meta',
+    {
+      name: 'keywords',
+      content:
+        '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown',
+    },
+  ],
+  // 移动浏览器主题颜色
+  ['meta', { name: 'theme-color', content: '#11a8cd' }],
+  // 移动端阻止页面缩放
+  [
+    'meta',
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no',
+    },
+  ],
+]
