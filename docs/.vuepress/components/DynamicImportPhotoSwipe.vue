dc9f7985fbbc8dcbf7a7bd758919541cf13cba3b
xiaojun996 <blueskyzx@icloud.com>
2020-11-08 22:23:33 +0800

    add: 添加PhotoSwipeVue

---
+++ docs/.vuepress/components/DynamicImportPhotoSwipe.vue
@@ -0,0 +1,30 @@
+<template>
+  <component
+    v-if="dynamicComponent"
+    :is="dynamicComponent"
+    v-bind="{
+      items: items,
+      options: { shareEl: false },
+    }"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      dynamicComponent: null,
+    }
+  },
+  async mounted() {
+    const module = await import('vue-picture-swipe')
+    this.dynamicComponent = module.default
+  },
+}
+</script>
