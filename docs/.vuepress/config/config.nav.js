1a38a65d15f9a6d5f918aa57eb7f351ad341ca4f
xiaojun996 <blueskyzx@icloud.com>
2020-11-22 14:34:02 +0800

    chore: 整理

--- docs/.vuepress/config/config.nav.js
+++ docs/.vuepress/config/config.nav.js
@@ -1,35 +1,43 @@
 module.exports = [
   {
-    text: '前端',
+    text: '笔记',
     items: [
-      { text: 'JavaScript笔记', link: '/pages/1e2d74/' },
-      { text: 'Vue笔记', link: '/pages/4331e9/' },
-      { text: 'React笔记', link: '/pages/6e530c/' },
-      { text: 'CSS笔记', link: '/pages/b9190a/' },
+      { text: 'JavaScript', link: '/pages/1e2d74/' },
+      { text: 'CSS', link: '/pages/b9190a/' },
+      { text: 'Vue', link: '/pages/4331e9/' },
+      { text: 'React', link: '/pages/6e530c/' },
     ],
   },
-  { text: 'Arithmetic', link: '/pages/c8d0c9/' },
   {
-    text: 'Document',
+    text: '算法',
     items: [
-      { text: 'Hackintosh', link: '/pages/b8569d/' },
+      { text: 'LeetCode', link: '/pages/c8d0c9/' },
+      { text: '排序算法', link: '/pages/9ec540/' },
+      { text: 'Other', link: '/pages/32cabf/' },
+    ],
+  },
+  {
+    text: '文档',
+    items: [
+      { text: 'Bash', link: '/pages/e89ffe/' },
       { text: 'ECMAScript6', link: '/pages/65966f/' },
-      { text: 'Bash脚本', link: '/pages/e89ffe/' },
+      { text: 'Hackintosh', link: '/pages/b8569d/' },
     ],
   },
+  { text: '片刻', link: '/pages/be163a/' },
   {
-    text: 'Indexed',
+    text: '索引',
     items: [
-      { text: 'Archives', link: '/archives/' },
-      { text: 'Categories', link: '/categories/' },
-      { text: 'Tags', link: '/tags/' },
+      { text: '档案', link: '/archives/' },
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
     ],
   },
   {
     text: 'Contact',
     items: [
-      { text: '掘金', link: 'https://juejin.im/user/2436173499732936', target: '_blank' },
       { text: 'GitHub', link: 'https://github.com/xiaojun996/', target: '_blank' },
+      { text: '掘金', link: 'https://juejin.im/user/2436173499732936', target: '_blank' },
       { text: 'Twitter', link: 'https://twitter.com/Luka_ai', target: '_blank' },
     ],
   },
